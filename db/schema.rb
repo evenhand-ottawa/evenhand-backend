d97e3a8a4d23463328b36df3dd301691f5f8f284
neil.mispelaar <neil.mispelaar@outlook.com>
2019-10-26 18:44:41 -0400

    Add User reference to speakers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_220929) do
+ActiveRecord::Schema.define(version: 2019_10_26_224136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2019_10_26_220929) do
     t.text "biography"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_speakers_on_user_id"
   end
 
   create_table "topics", force: :cascade do |t|
