3ea44fe47e27f35ee465b036d5bae775a89d39f0
Tai Dickerson <19917125+tdickers@users.noreply.github.com>
2019-10-26 19:53:28 -0400

    Reinit scaffolding for additional models

--- spec/controllers/speaker_languages_controller_spec.rb
+++ spec/controllers/speaker_languages_controller_spec.rb
@@ -26,7 +26,7 @@ require 'rails_helper'
 RSpec.describe SpeakerLanguagesController, type: :controller do
 
   # This should return the minimal set of attributes required to create a valid
-  # Speakerlanguage. As you add validations to Speakerlanguage, be sure to
+  # SpeakerLanguage. As you add validations to SpeakerLanguage, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
     skip("Add a hash of attributes valid for your model")
@@ -43,7 +43,7 @@ RSpec.describe SpeakerLanguagesController, type: :controller do
 
   describe "GET #index" do
     it "returns a success response" do
-      speakerlanguage = Speakerlanguage.create! valid_attributes
+      speaker_language = SpeakerLanguage.create! valid_attributes
       get :index, params: {}, session: valid_session
       expect(response).to be_successful
     end
@@ -51,33 +51,33 @@ RSpec.describe SpeakerLanguagesController, type: :controller do
 
   describe "GET #show" do
     it "returns a success response" do
-      speakerlanguage = Speakerlanguage.create! valid_attributes
-      get :show, params: {id: speakerlanguage.to_param}, session: valid_session
+      speaker_language = SpeakerLanguage.create! valid_attributes
+      get :show, params: {id: speaker_language.to_param}, session: valid_session
       expect(response).to be_successful
     end
   end
 
   describe "POST #create" do
     context "with valid params" do
-      it "creates a new Speakerlanguage" do
+      it "creates a new SpeakerLanguage" do
         expect {
-          post :create, params: {speakerlanguage: valid_attributes}, session: valid_session
-        }.to change(Speakerlanguage, :count).by(1)
+          post :create, params: {speaker_language: valid_attributes}, session: valid_session
+        }.to change(SpeakerLanguage, :count).by(1)
       end
 
-      it "renders a JSON response with the new speakerlanguage" do
+      it "renders a JSON response with the new speaker_language" do
 
-        post :create, params: {speakerlanguage: valid_attributes}, session: valid_session
+        post :create, params: {speaker_language: valid_attributes}, session: valid_session
         expect(response).to have_http_status(:created)
         expect(response.content_type).to eq('application/json')
-        expect(response.location).to eq(speaker_language_url(Speakerlanguage.last))
+        expect(response.location).to eq(speaker_language_url(SpeakerLanguage.last))
       end
     end
 
     context "with invalid params" do
-      it "renders a JSON response with errors for the new speakerlanguage" do
+      it "renders a JSON response with errors for the new speaker_language" do
 
-        post :create, params: {speakerlanguage: invalid_attributes}, session: valid_session
+        post :create, params: {speaker_language: invalid_attributes}, session: valid_session
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to eq('application/json')
       end
@@ -90,27 +90,27 @@ RSpec.describe SpeakerLanguagesController, type: :controller do
         skip("Add a hash of attributes valid for your model")
       }
 
-      it "updates the requested speakerlanguage" do
-        speakerlanguage = Speakerlanguage.create! valid_attributes
-        put :update, params: {id: speakerlanguage.to_param, speakerlanguage: new_attributes}, session: valid_session
-        speakerlanguage.reload
+      it "updates the requested speaker_language" do
+        speaker_language = SpeakerLanguage.create! valid_attributes
+        put :update, params: {id: speaker_language.to_param, speaker_language: new_attributes}, session: valid_session
+        speaker_language.reload
         skip("Add assertions for updated state")
       end
 
-      it "renders a JSON response with the speakerlanguage" do
-        speakerlanguage = Speakerlanguage.create! valid_attributes
+      it "renders a JSON response with the speaker_language" do
+        speaker_language = SpeakerLanguage.create! valid_attributes
 
-        put :update, params: {id: speakerlanguage.to_param, speakerlanguage: valid_attributes}, session: valid_session
+        put :update, params: {id: speaker_language.to_param, speaker_language: valid_attributes}, session: valid_session
         expect(response).to have_http_status(:ok)
         expect(response.content_type).to eq('application/json')
       end
     end
 
     context "with invalid params" do
-      it "renders a JSON response with errors for the speakerlanguage" do
-        speakerlanguage = Speakerlanguage.create! valid_attributes
+      it "renders a JSON response with errors for the speaker_language" do
+        speaker_language = SpeakerLanguage.create! valid_attributes
 
-        put :update, params: {id: speakerlanguage.to_param, speakerlanguage: invalid_attributes}, session: valid_session
+        put :update, params: {id: speaker_language.to_param, speaker_language: invalid_attributes}, session: valid_session
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to eq('application/json')
       end
@@ -118,11 +118,11 @@ RSpec.describe SpeakerLanguagesController, type: :controller do
   end
 
   describe "DELETE #destroy" do
-    it "destroys the requested speakerlanguage" do
-      speakerlanguage = Speakerlanguage.create! valid_attributes
+    it "destroys the requested speaker_language" do
+      speaker_language = SpeakerLanguage.create! valid_attributes
       expect {
-        delete :destroy, params: {id: speakerlanguage.to_param}, session: valid_session
-      }.to change(Speakerlanguage, :count).by(-1)
+        delete :destroy, params: {id: speaker_language.to_param}, session: valid_session
+      }.to change(SpeakerLanguage, :count).by(-1)
     end
   end
 
