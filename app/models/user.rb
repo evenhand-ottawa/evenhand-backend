52b60d03b3fff502c0314ef43c6d90fbd8ea6dfd
Tai Dickerson <19917125+tdickers@users.noreply.github.com>
2019-10-26 22:34:30 -0400

    Add scrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_one :speaker
+  before_validation :scrypt_password
+
+  def scrypt_password
+    if password_changed?
+      self.password = SCrypt::Password.create(self.password)
+    end
+  end
 end
